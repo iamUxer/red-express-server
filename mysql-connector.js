522df354c9d3a1a03aacd56b4db57b5909f807e0
guyuri <yulrhymz@gmail.com>
2022-01-24 19:45:20 +0900

    [#1] mysql install

---
+++ mysql-connector.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "red",
+});
+
+db.query("select 123 as abc", null, function (error, rows) {
+  console.log(error);
+  console.log(rows);
+});
+
+db.error = function (request, ressponse, error) {
+  console.error("Start: SQL error");
+  console.error(error.sql);
+  console.error("End: SQL error");
+  // error.sql = undefined;
+  ressponse.status(500).send(error);
+  return false;
+};
+
+module.exports = db;
