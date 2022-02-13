7019ced9d5144ed6863744568d05484a6933b107
guyuri <yulrhymz@gmail.com>
2022-02-13 17:54:28 +0900

    [#2] timezone: KST

--- mysql-connector.js
+++ mysql-connector.js
@@ -1,21 +1,22 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "red",
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'red',
+  timezone: 'KST',
 });
 
-db.query("select ? as ?", [12345, "abcdef"], function (error, rows) {
+db.query('select ? as ?', [12345, 'abcdef'], function (error, rows) {
   console.log(error);
   console.log(rows);
 });
 
 db.error = function (request, response, error) {
-  console.error("Start: SQL error");
+  console.error('Start: SQL error');
   console.error(error.sql);
-  console.error("End: SQL error");
+  console.error('End: SQL error');
   // error.sql = undefined;
   response.status(500).send(error);
   return false;
