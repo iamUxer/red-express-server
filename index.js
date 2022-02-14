6fccf0a3f9709192e8fa1d519d6c696f9cf1700c
guyuri <yulrhymz@gmail.com>
2022-02-14 20:59:57 +0900

    [#4] groceries

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-const express = (global.express = require("express"));
+const express = (global.express = require('express'));
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 // MySQL
-global.db = require("./mysql-connector.js");
+global.db = require('./mysql-connector.js');
 
 // Swagger
-const swaggerUi = require("swagger-ui-express");
+const swaggerUi = require('swagger-ui-express');
 app.use(
-  "/api-docs",
+  '/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(require("./swagger.json"))
+  swaggerUi.setup(require('./swagger.json'))
 );
 
 // Mock data
 global.mocks = {
-  members: require("./mocks/members.json"),
+  members: require('./mocks/members.json'),
 };
 
 // CROSS, Methods, Headers
 app.use(function (request, response, next) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, PUT, PATCH, DELETE'
   );
-  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
 // API router
 app.use(express.json());
-app.use("/api/v1/members", require("./routes/members.js"));
-app.use("/api/v1/search", require("./routes/search.js"));
-app.use("/api/v1/items", require("./routes/items.js"));
+app.use('/api/v1/members', require('./routes/members.js'));
+app.use('/api/v1/search', require('./routes/search.js'));
+app.use('/api/v1/items', require('./routes/items.js'));
+app.use('/api/v1/groceries', require('./routes/groceries.js'));
 
 // Start server
-global.location = new URL("http://localhost:3100");
+global.location = new URL('http://localhost:3100');
 app.listen(global.location.port, function () {
   console.log(
-    "Express server listening on " + global.location.origin + "/api/v1/members"
+    'Express server listening on ' + global.location.origin + '/api/v1/members'
   );
-  console.log("Swagger on " + global.location.origin + "/api-docs");
+  console.log('Swagger on ' + global.location.origin + '/api-docs');
 });
