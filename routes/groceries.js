b238832113597c7bf28b8b83badf0b3d10ba21f3
guyuri <yulrhymz@gmail.com>
2022-03-14 21:23:05 +0900

    [#10] jwt-middleware 적용

--- routes/groceries.js
+++ routes/groceries.js
@@ -2,15 +2,17 @@ const router = global.express.Router();
 const db = global.db;
 const jwtAuth = require('../middlewares/jwtAuth.js');
 
-router.post('/', function (request, response) {
+router.post('/', jwtAuth.tokenCheck, function (request, response) {
   const id = request.body.id;
   const sql = `
   insert into groceries (
     select item_pk as grocery_pk, member_pk, name, enter, expire from items
-    where item_pk = ?
+    where 
+      member_pk = ?
+      and item_pk = ?
   );
   `;
-  db.query(sql, [id], function (error, rows) {
+  db.query(sql, [request.decoded.member_pk, id], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Done groceries post', rows);
       response.status(200).send({
@@ -27,11 +29,12 @@ router.get('/', jwtAuth.tokenCheck, function (request, response) {
   const sql = `
       select * from groceries
       where
-        member_pk = 1
+        member_pk = ?
         and (? = '' or name like '%${q}%' )
       order by ${orderByName} ${orderByType};
     `;
-  db.query(sql, [q], function (error, rows) {
+  console.log(request.decoded);
+  db.query(sql, [request.decoded.member_pk, q], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Done groceries get');
       response.status(200).send({
@@ -42,13 +45,16 @@ router.get('/', jwtAuth.tokenCheck, function (request, response) {
   });
 });
 
-router.delete('/:id', function (request, response) {
+router.delete('/:id', jwtAuth.tokenCheck, function (request, response) {
   const id = Number(request.params.id);
   console.log('router delete id:::', id);
   const sql = `
-    delete from groceries where grocery_pk = ${id};
+    delete from groceries
+    where
+      member_pk = ?
+      and grocery_pk = ${id};
     `;
-  db.query(sql, null, function (error, rows) {
+  db.query(sql, [request.decoded.member_pk], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Succeed Delete grocery');
       response.status(200).send({
@@ -59,13 +65,20 @@ router.delete('/:id', function (request, response) {
   });
 });
 
-router.patch('/:index', function (request, response) {
+router.patch('/:index', jwtAuth.tokenCheck, function (request, response) {
   const index = Number(request.params.index);
   const body = request.body;
   const sql = `
-    update groceries set name = '${body.name}', enter = '${body.enter}', expire = '${body.expire}' where grocery_pk = ${index};
+    update groceries
+    set
+      name = '${body.name}',
+      enter = '${body.enter}',
+      expire = '${body.expire}'
+    where
+      member_pk = ?
+      and grocery_pk = ${index};
   `;
-  db.query(sql, null, function (error, rows) {
+  db.query(sql, [request.decoded.member_pk], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Succeed Update grocery', rows);
       response.status(200).send({
--- routes/items.js
+++ routes/items.js
@@ -1,27 +1,32 @@
 const router = global.express.Router();
 const db = global.db;
+const jwtAuth = require('../middlewares/jwtAuth.js');
 
-router.post('/', function (request, response) {
+router.post('/', jwtAuth.tokenCheck, function (request, response) {
   const sql = `
     insert into items(member_pk, name, enter, expire)
     values (
-      1,
+      ?,
       ?,
       date_format(now(), '%Y-%m-%d'),
       date_format(date_add(now(), interval + 2 week), '%Y-%m-%d')
     );
   `;
-  db.query(sql, [request.body.name], function (error, rows) {
-    if (!error || db.error(request, response, error)) {
-      console.log('Done items post', rows);
-      response.status(200).send({
-        result: 'Created',
-      });
+  db.query(
+    sql,
+    [request.decoded.member_pk, request.body.name],
+    function (error, rows) {
+      if (!error || db.error(request, response, error)) {
+        console.log('Done items post', rows);
+        response.status(200).send({
+          result: 'Created',
+        });
+      }
     }
-  });
+  );
 });
 
-router.get('/', function (request, response) {
+router.get('/', jwtAuth.tokenCheck, function (request, response) {
   const orderByName = request.query.orderByName || 'name';
   const orderByType = request.query.orderByType || 'asc';
   console.log('orderByName: ', orderByName, ', orderByType: ', orderByType);
@@ -31,10 +36,10 @@ router.get('/', function (request, response) {
           select grocery_pk from groceries g where g.grocery_pk = i.item_pk
         ) as grocery_pk
       from items i
-      where member_pk = 1
+      where member_pk = ?
       order by ${orderByName} ${orderByType};
     `;
-  db.query(sql, null, function (error, rows) {
+  db.query(sql, [request.decoded.member_pk], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       response.status(200).send({
         result: 'Readed',
@@ -44,12 +49,15 @@ router.get('/', function (request, response) {
   });
 });
 
-router.delete('/:index', function (request, response) {
+router.delete('/:index', jwtAuth.tokenCheck, function (request, response) {
   const index = Number(request.params.index);
   const sql = `
-    delete from items where item_pk = ${index};
+    delete from items
+    where
+      member_pk = ?
+      and item_pk = ${index};
     `;
-  db.query(sql, null, function (error, rows) {
+  db.query(sql, [request.decoded.member_pk], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Succeed Delete item', rows);
       response.status(200).send({
@@ -60,13 +68,20 @@ router.delete('/:index', function (request, response) {
   });
 });
 
-router.patch('/:index', function (request, response) {
+router.patch('/:index', jwtAuth.tokenCheck, function (request, response) {
   const index = Number(request.params.index);
   const body = request.body;
   const sql = `
-    update items set name = '${body.name}', enter = '${body.enter}', expire = '${body.expire}' where item_pk = ${index};
+    update items
+    set
+      name = '${body.name}',
+      enter = '${body.enter}',
+      expire = '${body.expire}'
+    where
+      member_pk = ?
+      and item_pk = ${index};
   `;
-  db.query(sql, null, function (error, rows) {
+  db.query(sql, [request.decoded.member_pk], function (error, rows) {
     if (!error || db.error(request, response, error)) {
       console.log('Succeed Update item', rows);
       response.status(200).send({
